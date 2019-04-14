37f470e7936b186caee79f692e886f745842eb85
Ismajl Ramadani <ismajl.ramadani@ornio.no>
2019-04-14 13:12:35 +0200

    substring match implemented

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # regexp-matcher
+
+    c    matches any literal character c
+    .    matches any single character
+    ^    matches the beginning of the input string
+    $    matches the end of the input string
+    *    matches zero or more occurrences of the previous character
+
+A Regular Expression Matcher, C code & Algorithm Description
+http://www.cs.princeton.edu/courses/archive/spr09/cos333/beautiful.html
--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log('hello world');
+function match (regexp, text) {
+    let i = 0;
+    do {
+        if (matchhere(regexp, text.substring(i)))
+            return 1;
+    } while (i++ < text.length);
+    return 0;
+}
+
+function matchhere (regexp, text) {
+    if (regexp.length === 0)
+        return 1;
+    if (text.length > 0 && (regexp[0] === text[0]))
+        return matchhere(regexp.substring(1), text.substring(1))
+    return 0;
+}
